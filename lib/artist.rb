58129b446854dea0b6f2d6ebfb2d60c47d62bb21
Johnathan <johnathanguzman17@gmail.com>
2020-04-05 13:36:02 -0500

    finished labs

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require "pry"
+require "yaml"
+
+class Artist
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all.push(self)
+    end
+
+    def songs
+      Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+      song.artist = self
+    end
+
+    def add_song_by_name(name)
+      song = Song.new(name)
+      song.artist = self
+    end
+  
+    def self.song_count
+      Song.all.length
+    end
+  end
+
+# binding.pry
